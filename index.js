84cb2809d8227be2db858bb3c7c9978180d21fc9
ghermidont <suntnascutin83>
2019-06-11 16:37:18 +0300

    Dublicate deletion algorithm

--- index.js
+++ index.js
@@ -1,75 +1,38 @@
-const main = ( iterable ) => {
+var main = function( iterable ){
+    var sortedArray = new Array();
     let stringValue = typeof iterable;
 
-    if( Array.isArray( iterable ) == true ){
-        arraySort( iterable );
-    }else if( stringValue === "string" ){
-        stringSort( iterable );
-    }else{
-        console.log( "Can not string nor array!" );
+    for ( let i = 0; i <= iterable.length-1; i++ ){
+        if( Array.isArray( iterable ) == true ){
+            sortedArray.push( iterable[i] );
+        }else if( stringValue === "string" ){
+            sortedArray.push( iterable.charAt(i) );
+        }else{
+            console.log( "Please input a string or an array!" );
+        }
     }
+    sortAndDisplaySortedArray(sortedArray);
 };
 
-var sortedArray = new Array();
-
-const stringSort = ( myValue )=>{
-    var character = "";
-    var singleChar = '';
-   
-    for ( var i = 0; i <= myValue.length-1; i++ ){
-        character = myValue.charAt(i);
-        sortAndAddtoSortedArray( character );
-    }
-    displayArray( sortedArray );
-};
-
-const arraySort = ( myValue ) => {
-    for ( var i = 0; i <= myValue.length-1; i++ ){
-        let character = myValue[i];
-        sortAndAddtoSortedArray( character );
-    }
-    displayArray( sortedArray );
-};
-
-
-const displayArray = function ( theArray ){
-    let y = 1
-    for( let i = 0; i <= theArray.length ; i++, y++ ){
-        if( theArray[y+1] != undefined ){
-            if( theArray[i] == theArray[y] ){
-                theArray.splice( y, 1 );
-            }
-         }
-
+var sortAndDisplaySortedArray = function( theArray ){
+    let y = 1;
+    for( let i = 0; i <= theArray.length-1 ; i++, y++ ){
+       if(theArray[i] == theArray[y] ){
+            theArray.splice( y, 1 );
+        }
     }
 
-    for( let n = 0; n <= theArray.length; n++ ){
+    for( let n = 0; n <= theArray.length-1; n++ ){
         console.log( theArray[n] );
-        if( theArray[n+1] == undefined )break;
     }
-       
-    
 }
 
-const sortAndAddtoSortedArray = function(character){
-    if ( typeof character != "number" ){
-        if ( character == character.toUpperCase() ) {
-            singleChar = character;
-            sortedArray.push( singleChar );
-        }else{
-            singleChar = character;
-            sortedArray.push( singleChar );
-        }
-    }else {
-
-        singleChar = character;
-        sortedArray.push( singleChar );
-    }
-};
-
 //test w/t Array
-main(['a', 'a', 'b', 'G', 'G', 'T', 'S', 'F']);
+//main([1, 1, 'd', 6, 9, 9, 10, 4, 5]);
 
 //test w/t string
-main("aabGGTSF");
+//main("aabGGTSFF");
+
+
+
 
